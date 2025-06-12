e827a1cde4a57d499c67552e30554adbd3362afa
nashxH2003 <thurainhein.work@gmail.com>
2025-06-12 22:58:40 +0700

    Adding: Unit Convertor.

---
+++ lib/feature/unit_convertor/presentation/ui/widget/category_dropdown/category_dropdown.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tools_for_you/core/theme/app_color.dart';
+
+class CategoryDropdown extends ConsumerWidget {
+  const CategoryDropdown({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<String> category = [
+      'Width',
+      'Length',
+      'Weight',
+      'Time',
+    ];
+
+    String selectedCategory =
+        category.first; // For demo; should use Riverpod state
+
+    return DropdownButtonFormField<String>(
+      isExpanded: true,
+      value: selectedCategory,
+      icon: const Icon(Icons.arrow_drop_down),
+      menuMaxHeight: 200,
+      decoration: InputDecoration(
+        labelText: 'Choose Category',
+        floatingLabelStyle: TextStyle(color: AppColor.kPrimary),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: AppColor.kPrimary),
+        ),
+      ),
+      selectedItemBuilder: (context) {
+        return category.map((item) {
+          return Center(
+            child: Text(
+              item,
+              style: TextStyle(color: AppColor.kWhite),
+            ),
+          );
+        }).toList();
+      },
+      items: category.map((item) {
+        return DropdownMenuItem<String>(
+          value: item,
+          child: Center(
+            child: Text(
+              item,
+              style: TextStyle(color: AppColor.kBlack),
+            ),
+          ),
+        );
+      }).toList(),
+      onChanged: (value) {
+        // Update state here with Riverpod
+        if (value != null) {
+          // Example print
+          print('Selected: $value');
+        }
+      },
+    );
+  }
+}
